03ba7ece71da6f0cc39f564690a569e8fec53ed0
a51095693 <a51095693@gmail.com>
2026-06-16 23:13:41 +0800

    config: 填入 Firebase 設定

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "FILL_IN",
-  authDomain: "FILL_IN",
-  projectId: "FILL_IN",
-  storageBucket: "FILL_IN",
-  messagingSenderId: "FILL_IN",
-  appId: "FILL_IN"
+  apiKey: "AIzaSyDYqbcnkJ7U_feYaMY6UzIYQhF4-gt4i8Y",
+  authDomain: "complaint-system-d14d8.firebaseapp.com",
+  projectId: "complaint-system-d14d8",
+  storageBucket: "complaint-system-d14d8.firebasestorage.app",
+  messagingSenderId: "139100398274",
+  appId: "1:139100398274:web:591e1d4b0bd7bf5956115b"
 };
